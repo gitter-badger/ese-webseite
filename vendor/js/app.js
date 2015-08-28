0ad41355f6cbf1cffae30cf9876756dc97e30508
Operator <development@kepler.international>
2015-08-28 12:52:19 +0200

    + the developer version for gh-pages
    This adresses the issue #4 gh-pages. Could someone please review the changes and rename the branch if it is okay.
    I tried to do as much as I can on a static site to prevent search engines from crawling the site. Now you will need JS to look at the site and its content. (or you just manualy alter the css but okay)
    Additionally I added a reveal box with a message stating that this is just a developer site.

--- vendor/js/app.js
+++ vendor/js/app.js
@@ -1,7 +1,14 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).ready(function(){
+    /**
+     * Initialize Foundation JS
+     */
     $(document).foundation();
+    
+    /**
+     * Initialize Slick Slider
+     */
     $('.slider').slick({
         lazyLoad: 'ondemand',
         dots: false,
@@ -12,4 +19,26 @@ $(document).ready(function(){
         // Disable navigation arrows
         arrows: false
     });
+    
+    /**
+     * Test if a SE Bot tries to look at the site
+     * This is only for the development site gh-pages
+     */
+    if(/bot|googlebot|crawler|spider|robot|crawling/i.test(window.navigator.userAgent)) {
+	    $('html').removeAttr("class");
+	    $('html').addClass("crawler");
+	    window.location.replace("https://ese.ifsr.de");
+    }
+    
+    /**
+     * Developer Preview Message Reveal
+     */
+    if(!$( "html" ).hasClass( "crawler" )) {
+    	$('#developer-preview-message').foundation('reveal', 'open');
+    }
+    
+    $("#developer-preview-message-close").on('click', function() {
+  		$('#developer-preview-message').foundation('reveal', 'close');
+		});
+
 });
